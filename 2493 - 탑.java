ee022332bc0975e9e36dc3c9f638902af48f2b09
jooy311 <60565941+jooy311@users.noreply.github.com>
2020-03-19 18:19:24 +0900

    Update 2493 - 탑.java

--- 2493 - 탑.java
+++ 2493 - 탑.java
@@ -1,5 +1,3 @@
-package exam;
-
 import java.util.*;
 import java.util.regex.Pattern;
 import java.io.*;
@@ -16,7 +14,7 @@ class Pair2 {
 	}
 }
 
-public class solution3 {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -32,17 +30,22 @@ public class solution3 {
 
 		for (int i = 1; i <= n; i++) {
 			if (i == 1) {
-				System.out.print("0"+ " ");
+				System.out.print("0" + " ");
 				s.push(arr[i]);// 첫번째 건물을 스택에 넣는다
-				continue;//다음 i로 가도록
+				continue;// 다음 i로 가도록
 			}
 
-			if (s.peek().height < arr[i].height) {// 나보다 작으면 pop한다
-				s.pop();
+			if (s.peek().height <= arr[i].height) {// 나보다 작으면 pop한다
+				while (!s.isEmpty()) {
+					if (arr[i].height < s.peek().height)
+						break;
+					s.pop();//처음에 먼저pop해주고 조건문 걸었더니 emptystack오류 떠서 고생함 뒤에다 빼는게 맞음 먼저 확인하고->pop
+				}
+
 				if (s.isEmpty())// pop했는데 스택이 비어있다면 0출력
-					System.out.print("0"+ " ");
+					System.out.print("0" + " ");
 				else
-					System.out.print(s.peek().pos+ " ");
+					System.out.print(s.peek().pos + " ");
 				s.push(arr[i]);// 그리고 나를 넣는다.
 			} else {// 나보다 크다면 냅둔다.
 				System.out.print(s.peek().pos + " ");
