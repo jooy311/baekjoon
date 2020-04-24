9d19af73098dcf0642124203ecd1a7d437fa2581
jooy311 <60565941+jooy311@users.noreply.github.com>
2020-04-24 18:06:18 +0900

    Update #17822 -원판돌리기.java

--- #17822 -원판돌리기.java
+++ #17822 -원판돌리기.java
@@ -6,9 +6,11 @@ class Main {
    static class Pair {
       int x;
       int y;
-      public Pair(int x, int y){
+      int val;
+      public Pair(int x, int y, int val){
       	this.x = x;
       	this.y = y;
+      	this.val = val;
       }
    }
 
@@ -16,6 +18,7 @@ class Main {
    static int n;
    static int m;
    static int t;
+  // static int sum = 0;
    static int[] dx = {0,0,-1,1};
    static int[] dy = {-1,1,0,0};
 
@@ -28,6 +31,7 @@ class Main {
       t = Integer.parseInt(st.nextToken());// 회전하는 횟수
       // 원판을 t번 회전시킨 후 원판에 적힌 수의 합을 출력한다
       arr = new int[n + 1][m + 1];// 각원판에 적힌 수를 담기 위한 배열
+      int ans = 0;
       for (int i = 1; i <= n; i++) {
          st = new StringTokenizer(br.readLine());
          for (int j = 1; j <= n; j++) {
@@ -51,29 +55,21 @@ class Main {
          while (!q.isEmpty()) {
             spin(q.poll(), d, k);// 해당 방향으로 k만큼 돌리고
          }
-        // Queue<Pair> qq = new LinkedList<Pair>();
-        
+ 
 		nearby(1,1);
-			
-		
-        // nearby();// 인접한것을 찾는다
          
       }//while문 끝
-      
-      for(int i=1; i<=n; i++) {
-         for(int j=1; j<=n; j++) {
-            System.out.print(arr[i][j]);
-         }
-         System.out.println();
+      for(int i=1; i<=n; i++){
+      	for(int j=1; j<=n; j++){
+      		ans += arr[i][j];
+      	}
       }
-      
+      System.out.println(ans);
    }
 
    public static void spin(int wh, int dir, int k) {// 원판을 돌리는 함수->해당하는 판을 매개변수로 보내주면 dir매개변수 방향으로 돌려주면됨
-      // 한번도는 것을 기준으로 만든다(1번 이상은 main함수에서 작성)
       Deque<Integer> dq = new LinkedList<Integer>();
       for (int i = 1; i <= m; i++) {
-         //System.out.println(arr[wh][i]);
          dq.addFirst(arr[wh][i]);// 덱에 wh번째 원판에 있는 숫자를 넣는다
       }
       
@@ -85,7 +81,6 @@ class Main {
          while (!dq.isEmpty()) {
             for (int i = 1; i <= n; i++) {
                arr[wh][i] = dq.pollLast();
-               //System.out.print(dq.pollFirst());
             }
          }
       }
@@ -103,26 +98,53 @@ class Main {
 
    }
 
+//여기서 자꾸 시간 초과 남
    public static void nearby(int a, int b) {//bfs로 인접한 같은 숫자 삭제
 	Queue<Pair> q1 = new LinkedList<Pair>();
 	boolean[][] check = new boolean[n+1][m+1];//같아서 삭제가 되면 true바꿔서 못들리게
-	q1.add(new Pair(a,b));
+	boolean flag = false;
+	int sum = 0;
+	int cnt = 0;
+	
+	q1.add(new Pair(a,b,arr[a][b]));
 	while(!q1.isEmpty()){
 		Pair p = q1.poll();
+		if(p.val == 0) continue;//0인애들은 걸러
 		int x = p.x;
 		int y = p.y;
+		int val = p.val;
+		sum += val;
+		cnt += 1;
+		check[x][y] = true;
 		for(int k=0; k<4; k++){
 			int nx = x + dx[k];
 			int ny = y + dy[k];
 			if(nx >0 && ny > 0  && nx <= n && ny <= m){
-				if(arr[x][y] == arr[nx][ny]){
+				if(val == arr[nx][ny] && check[nx][ny] == false){
+					check[nx][ny] = true;//방문함
+					q1.add(new Pair(nx,ny,arr[nx][ny]));
+					flag = true;
 					arr[x][y] = 0;
 					arr[nx][ny] = 0; //0으로 삭제 시켜준다
-				}else
-					q1.add(new Pair(nx,ny));
+				}
+			}
+		}
+	}//while끝
+	
+	if(flag == false){//한번도 같은 숫자를 찾지 못했다는 뜻
+		//평균을 구한다
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				if(arr[i][j] != 0){
+					if(arr[i][j] > sum/cnt) 
+						arr[i][j] += 1;
+					else if(arr[i][j] < sum/cnt)
+						arr[i][j] -= 1;
+				}
 			}
 		}
 	}
+	//retrun sum;
    }
 
 }
