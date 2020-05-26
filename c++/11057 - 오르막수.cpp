f973a8d3d22974cb8930fd32e049b374139dc5f7
jooy311 <60565941+jooy311@users.noreply.github.com>
2020-05-26 22:10:54 +0900

    Create 11057 - 오르막수.cpp

---
+++ c++/11057 - 오르막수.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int n;
+
+int main() {
+	cin >> n;
+	int** dp = new int*[n + 1];
+	memset(dp, 0, sizeof(n + 1));
+
+	for (int i = 0; i < n + 1; i++) {
+		dp[i] = new int[9];
+		memset(dp[i], 0, sizeof(int) * 9);
+	}
+
+	for (int i = 0; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= 9; j++) {
+			for (int k = j; k <= 9; k++) {
+				dp[i][j] += dp[i - 1][k];
+				dp[i][j] %= 10007;
+			}
+		}
+	}
+	int sum = 0;
+	for (int i = 0; i <= 9; i++) {
+		sum += dp[n][i];
+	}
+	cout << sum % 10007 << "\n";
+	
+	return 0;
+}
